8cb94a4b86cd45e42c5e94a38d3ec9dc4c4a6a1c
VashisthDev <107829512+VashisthDev@users.noreply.github.com>
2023-08-16 17:23:15 +0530

    *

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -7,16 +7,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
-  connectedAccount = '0x3678723angj';
-  isWalletConnected = true;
   
   constructor(private router: Router) { }
 
-  connectWallet(): void {
-    const extensionId = 'ebjabocgmjhfnlngeimpmapmkdjnioop';
-    window.open('templewallet://open', '_blank');
-
-  }
+  
   
   onSubmit(form: any) {
     this.router.navigate(['search', form.value.search]);
